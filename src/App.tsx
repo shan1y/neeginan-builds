c404b7fb5311be65c0a8b3dc1f40110eb13ba96f
Shannon Yazdani <shannonyazdani@gmail.com>
2022-10-06 23:04:13 -0400

    Removed unused components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import "./App.scss";
 import About from "./components/About/About";
 import Hero from "./components/Hero/Hero";
-import PageHeader from "./components/PageHeader/PageHeader";
 import Portfolio from "./components/Portfolio/Portfolio";
 
 function App() {
